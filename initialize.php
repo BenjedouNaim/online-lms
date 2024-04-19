8e27fd0ca169c7208847d5a4da4dd20155dd0c82
Nviim <naim.benj@gmail.com>
2024-04-19 06:53:13 +0100

    v1

---
+++ initialize.php
@@ -0,0 +1,10 @@
+<?php
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://pfa.rf.gd/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"sql300.infinityfree.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"if0_36078681");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"VOaf5GtZWUU");
+if(!defined('DB_NAME')) define('DB_NAME',"if0_36078681_leave_db");
+?>
